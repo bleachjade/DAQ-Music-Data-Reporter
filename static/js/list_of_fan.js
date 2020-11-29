cf2172da238d613f1d5729fe6cf891749afd2a5c
bleachjade <bleachjade@hotmail.com>
2020-11-29 20:38:16 +0700

    refactor chart

--- static/js/list_of_fan.js
+++ static/js/list_of_fan.js
@@ -1,5 +1,16 @@
-window.onload = function () {
-	var chart = new CanvasJS.Chart("chartContainer", {
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://feen:feen@cluster0.mo20d.mongodb.net/Song?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("MusicDataReporter");
+  dbo.collection("Artists").find({}, { projection: { _id: 0, artist: 1, number_of_fans: 1 } }).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+
+    var dps = [];
+
+    var chart = new CanvasJS.Chart("chartContainer", {
 		title:{
 			text: "Music Data API : List of Fans"              
 		},
@@ -8,15 +19,22 @@ window.onload = function () {
 			// Change type to "doughnut", "line", "splineArea", etc.
 			type: "column",
 			dataPoints: [
-				{ label: "Blackpink",  y: 100  },
-				{ label: "Justin Bieber", y: 15  },
-				{ label: "IZ*ONE", y: 25  },
-				{ label: "SNSD",  y: 30  },
-				{ label: "Tobu",  y: 28  }
+				function parseDataPoints () {
+                    for (var i = 0; i <= result.length; i++)
+                        for(var j = 0; j <= i.length; j++)
+                            dps.push({label: result[i][j], y: result[i][j+1]});
+                            console.log(dps)  
+                };
+                
 			]
 		}
 		]
-	});
-	chart.render();
-
-}
+    });
+    parseDataPoints();
+    chart.options.data[0].dataPoints = dps;
+    
+    chart.render();
+    
+    db.close();
+  });
+});
