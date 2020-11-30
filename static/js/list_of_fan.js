5ad64c60c71f724febefa67e2959d13ea02c814b
bleachjade <bleachjade@hotmail.com>
2020-11-30 14:39:09 +0700

    init heroku

--- static/js/list_of_fan.js
+++ static/js/list_of_fan.js
@@ -7,34 +7,7 @@ MongoClient.connect(url, function(err, db) {
   dbo.collection("Artists").find({}, { projection: { _id: 0, artist: 1, number_of_fans: 1 } }).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
-
-    var dps = [];
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-		title:{
-			text: "Music Data API : List of Fans"              
-		},
-		data: [              
-		{
-			// Change type to "doughnut", "line", "splineArea", etc.
-			type: "column",
-			dataPoints: [
-				function parseDataPoints () {
-                    for (var i = 0; i <= result.length; i++)
-                        for(var j = 0; j <= i.length; j++)
-                            dps.push({label: result[i][j], y: result[i][j+1]});
-                            console.log(dps)  
-                };
-                
-			]
-		}
-		]
-    });
-    parseDataPoints();
-    chart.options.data[0].dataPoints = dps;
-    
-    chart.render();
-    
     db.close();
   });
 });
+
